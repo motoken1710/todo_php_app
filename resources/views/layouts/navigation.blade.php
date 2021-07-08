9051bb5632a3c72714dac885e7723c4574389d15
motoken1710 <hugusanma@gmail.com>
2021-07-08 13:03:01 +0000

    add-common-page

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,6 +16,18 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
+                <!-- ↓一覧ページへのリンクを追加 -->
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('todo.index')" :active="request()->routeIs('todo.index')">
+                     {{ __('Index') }}
+                    </x-nav-link>
+                 </div>
+                <!-- ↓作成ページへのリンクを追加 -->
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                  <x-nav-link :href="route('todo.create')" :active="request()->routeIs('todo.create')">
+                    {{ __('Create') }}
+                  </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
@@ -67,7 +79,18 @@
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
         </div>
-
+        <!-- ↓一覧ページへのリンクを追加 -->
+    <div class="pt-2 pb-3 space-y-1">
+      <x-responsive-nav-link :href="route('todo.index')" :active="request()->routeIs('todo.index')">
+        {{ __('Index') }}
+      </x-responsive-nav-link>
+    </div>
+        <!-- ↓作成ページへのリンクを追加 -->
+    <div class="pt-2 pb-3 space-y-1">
+      <x-responsive-nav-link :href="route('todo.create')" :active="request()->routeIs('todo.create')">
+        {{ __('Create') }}
+      </x-responsive-nav-link>
+    </div>
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200">
             <div class="px-4">
